5debf9dd19517e7f773bd4c019db0c6219a2af53
discphy <dinophy@nate.com>
2022-08-12 11:43:05 +0900

    build(create): Init Project

---
+++ src/test/java/com/discphy/openkakao/OpenKakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discphy.openkakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenKakaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
